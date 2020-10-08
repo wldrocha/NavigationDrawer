5517c5e4cc8730af6a241177964712dd74eeec8f
wladimir Rocha <wladimir.rocha@equilibrio.agency>
2020-10-08 18:47:39 -0400

    open drawer

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.navigationdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolbar();
+        drawerLayout = findViewById(R.id.drawerLayout);
+    }
+
+    private void setToolbar(){
+        setSupportActionBar((androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar));
+        //Flecha atrás o icono
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Opciones para activar el nav_menu con
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //abrir el nav_menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
